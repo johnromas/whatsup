7bd81fadc86b7c979ca9c8e1fbef2ec239afec8d
John Romas <john.romas87@gmail.com>
2014-07-24 20:02:36 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Whatsup::Application.config.secret_key_base = '08bbfb3d35cd1ec25808408e70f35ab035c99f169e6c5d2d46625924d796215d3689c743c81569571e33447a33cbe330cbac9e23db1f46294fd47966f90bdb0e'

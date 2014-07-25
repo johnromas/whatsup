b4d0e6b6e49e87367edf6baa4a28f3a19c1c8fb7
vagrant <vagrant@rails-dev-box>
2014-07-25 00:53:43 +0000

    Added Devise and created associations for users/events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,4 @@
 class Event < ActiveRecord::Base
+	has_many :attendences
+	has_many :users, through: :attendences
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140725000757) do
+ActiveRecord::Schema.define(version: 20140725003715) do
+
+  create_table "attendences", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "event_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "attendences", ["event_id"], name: "index_attendences_on_event_id"
+  add_index "attendences", ["user_id"], name: "index_attendences_on_user_id"
 
   create_table "events", force: true do |t|
     t.string   "name"
@@ -24,5 +34,25 @@ ActiveRecord::Schema.define(version: 20140725000757) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "users", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
